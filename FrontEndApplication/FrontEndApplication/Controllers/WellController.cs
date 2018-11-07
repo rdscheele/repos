3741bf1f61ab270fe584062b719082642bdd30ec
Rodi Scheele <r.d.scheele@betabit.nl>
2018-11-07 13:49:27 +0100

    added BLOB storage account functionality

--- FrontEndApplication/FrontEndApplication/Controllers/WellController.cs
+++ FrontEndApplication/FrontEndApplication/Controllers/WellController.cs
@@ -20,6 +20,7 @@ namespace FrontEndApplication.Controllers
         const string ServiceBusConnectionString = "Endpoint=sb://wellprototype.servicebus.windows.net/;SharedAccessKeyName=master;SharedAccessKey=2uWqgYUl+0PZerFo4qrVPLj1pOiaZGUHDDnXc8I8Umg=";
         const string QueueName = "wellqueue";
         static IQueueClient queueClient;
+        const string StorageAccountConnectionString = "DefaultEndpointsProtocol=https;AccountName=bbwelldata;AccountKey=fK2i08pNdKOB6nHQYwvvS42EJBegLRlHvqz3IBKsxMkVhkfjmNQJMLzZjFJ71Mnf5+G67t4ajR6usvHLNJVilA==;EndpointSuffix=core.windows.net";
 
         // Portal page
         public IActionResult Index()
@@ -30,14 +31,16 @@ namespace FrontEndApplication.Controllers
         // Portal page after a message is send
         public IActionResult SendMessage()
         {
-            MainAsync().GetAwaiter().GetResult();
-            ViewBag.Result = "Message send!";
+            Random rnd = new Random();
+            int numberOfMessages = rnd.Next(1, 20);
+
+            MainAsync(numberOfMessages).GetAwaiter().GetResult();
+            ViewBag.Result = "A total of " + numberOfMessages + " messages have been send!";
             return View("Index");
         }
 
-        private static async Task MainAsync()
+        private static async Task MainAsync(int numberOfMessages)
         {
-            const int numberOfMessages = 10;
             queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
 
             // Send messages.
@@ -46,12 +49,15 @@ namespace FrontEndApplication.Controllers
             await queueClient.CloseAsync();
         }
 
+        // Send and upload messages
         private static async Task SendMessagesAsync(int numberOfMessagesToSend)
         {
             Random rnd = new Random();
             // Create storage container for the incoming messages
             string subDomain = rnd.Next(1000000, 9999999).ToString();
-            //CreateStorageContainer(subDomain);
+
+            // Create a blob container to save the message in
+            await CreateBlobContainer(subDomain);
 
             try
             {
@@ -59,11 +65,12 @@ namespace FrontEndApplication.Controllers
                 {
                     // Create a new message to send to the queue.
                     string messageId = rnd.Next(10000, 99999).ToString();
-                    string messageBody = $"Message {i} ;" + messageId;
-                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
-                    Console.WriteLine(messageBody);
 
-                    //SaveToBlob(subDomain, messageId);
+                    // Save the image to the blob storage (Don't need to await this)
+                    SaveToBlobAsync(subDomain, messageId);
+
+                    // Generate the message for the service bus queue
+                    Message message = GenerateQueueMessage(subDomain, messageId, numberOfMessagesToSend);
 
                     // Send the message to the queue.
                     await queueClient.SendAsync(message);
@@ -77,8 +84,17 @@ namespace FrontEndApplication.Controllers
             }
         }
 
+        // Generate the contents of the message
+        private static Message GenerateQueueMessage(string subDomain, string messageId, int numberOfMessagesToSend)
+        {
+            string messageBody = subDomain + ";" + messageId + ".jpeg" + ";" + numberOfMessagesToSend.ToString();
+            Message message = new Message(Encoding.UTF8.GetBytes(messageBody));
+            Console.WriteLine("Message body; " + messageBody);
+            return message;
+        }
+
         // Add one file to the blob
-        private static void SaveToBlob(string subDomain, string messageId)
+        private static async Task SaveToBlobAsync(string subDomain, string messageId)
         {
             // Rename the file so that the filename equals the messageId
             string file = GetRandomFile();
@@ -86,7 +102,7 @@ namespace FrontEndApplication.Controllers
             System.IO.File.Move(file, newFileName);
 
             // Retrieve storage account from connection string.
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("fK2i08pNdKOB6nHQYwvvS42EJBegLRlHvqz3IBKsxMkVhkfjmNQJMLzZjFJ71Mnf5+G67t4ajR6usvHLNJVilA==");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(StorageAccountConnectionString);
 
             // Create the blob client.
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
@@ -95,14 +111,25 @@ namespace FrontEndApplication.Controllers
             CloudBlobContainer container = blobClient.GetContainerReference(subDomain);
 
             // Retrieve reference to a blob named "myblob".
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference(newFileName);
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(messageId + ".jpeg");
 
             // Create or overwrite the "myblob" blob with contents from a local file.
-            using (var fileStream = System.IO.File.OpenRead(@"path\myfile"))
-            {
-                //blockBlob.UploadFromStream(fileStream);
-            }
+            await blockBlob.UploadFromFileAsync(newFileName);
+        }
 
+        private static async Task CreateBlobContainer(string subDomain)
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(StorageAccountConnectionString);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference(subDomain);
+
+            //Create a new container, if it does not exist
+            await container.CreateIfNotExistsAsync();
         }
 
         // Select a random file from the ./Resources/ folder
@@ -115,22 +142,6 @@ namespace FrontEndApplication.Controllers
                                  .Take(1)
                                  .FirstOrDefault();
             return file;
-        }
-
-        // Creates a storage container
-        private static void CreateStorageContainer(string subDomain)
-        {
-            // Retrieve storage account from connection string.
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("fK2i08pNdKOB6nHQYwvvS42EJBegLRlHvqz3IBKsxMkVhkfjmNQJMLzZjFJ71Mnf5+G67t4ajR6usvHLNJVilA==");
-
-            // Create the blob client.
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-
-            //the container for this is companystyles
-            CloudBlobContainer container = blobClient.GetContainerReference(subDomain);
-
-            //Create a new container, if it does not exist
-            container.CreateIfNotExistsAsync();
         }
     }
 }
